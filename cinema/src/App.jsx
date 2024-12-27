07c747f1cf0de89002f6a284375625831038d398
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2024-12-27 11:53:34 +0530

    search functionality

---
+++ cinema/src/App.jsx
@@ -0,0 +1,13 @@
+import MovieCard from "./components/MovieCard";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ cinema/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movieList = [
+    { id: 1, title: "Thuppakki", release_date: "2012" },
+    { id: 2, title: "Kaththi", release_date: "2014" },
+    { id: 3, title: "Theri", release_date: "2017" },
+    { id: 4, title: "Mersal", release_date: "2018" },
+    { id: 5, title: "Master", release_date: "2021" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery("");
+  };
+  return (
+    <div className="home">
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for Movies.."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movieList.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
